bd8f92a6ceff1fd30eaffeafbf2cb90d9c188bcc
Charles Kenney <Charlesc.kenney@gmail.com>
2017-09-08 00:03:08 -0400

    update tests

--- test.js
+++ test.js
@@ -4,11 +4,11 @@ import fn from './';
 test('should scrape ~sindresorhus', async t => {
 	const user = await fn('sindresorhus');
 
-	t.is(user.name, null);
+	t.is(user.name, 'Sindre Sorhus');
 	t.is(user.email, 'sindresorhus@gmail.com');
-	t.is(user.homepage, null);
-	t.is(user.github, null);
-	t.is(user.twitter, null);
+	t.is(user.homepage, 'https://sindresorhus.com');
+	t.is(user.github, 'sindresorhus');
+	t.is(user.twitter, 'sindresorhus');
 	t.is(user.freenode, null);
 });
 
