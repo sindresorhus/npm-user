0f43ef9ac0833c833c640bbcc806348d32008141
tbozhikov <t_bozhikov@yahoo.com>
2017-11-15 16:33:34 +0200

    populate email using npm-email

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 const got = require('got');
 const cheerio = require('cheerio');
+const npmEmail = require('npm-email');
 
 module.exports = username => {
 	if (typeof username !== 'string') {
@@ -8,22 +9,28 @@ module.exports = username => {
 	}
 
 	const url = `https://www.npmjs.com/~${username}`;
-
-	return got(url).then(res => {
-		const $ = cheerio.load(res.body);
+    
+    return Promise.all([got(url), npmEmail(username)]).then((values) => {
+        const res = values[0];
+        const email = values[1];
+        const $ = cheerio.load(res.body);
 
 		let avatar = $('.avatar img').attr('src');
 		avatar = avatar ? avatar.replace(/^(https:\/\/)s\./, '$1').replace(/&default=retro$/, '') : null;
 
-		return {
+        console.log(res);
+        console.log($('.email [data-email]').attr('data-email'));
+
+        return {
 			name: $('.fullname').text() || null,
 			avatar,
+			email: email || null,
 			homepage: $('.homepage a').attr('href') || null,
 			github: $('.github a').text().slice(1) || null,
 			twitter: $('.twitter a').text().slice(1) || null,
 			freenode: $('.freenode a').text() || null
 		};
-	}).catch(err => {
+    }).catch(err => {
 		if (err.statusCode === 404) {
 			err.message = 'User doesn\'t exist';
 		}
