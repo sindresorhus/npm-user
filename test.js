2c892b3093566f141a13eebf2d11c107613bd83d
Zeke Sikelianos <zeke@sikelianos.com>
2018-03-21 20:22:53 -0700

    update to account for new npm website

--- test.js
+++ test.js
@@ -3,14 +3,11 @@ import m from '.';
 
 test('user: sindresorhus', async t => {
 	const user = await m('sindresorhus');
-	console.log(user);
 	t.is(user.name, 'Sindre Sorhus');
 	t.regex(user.avatar, /gravatar\.com\/avatar/);
 	t.is(user.email, 'sindresorhus@gmail.com');
-	t.is(user.homepage, 'https://sindresorhus.com');
 	t.is(user.github, 'sindresorhus');
 	t.is(user.twitter, 'sindresorhus');
-	t.is(user.freenode, null);
 });
 
 test('user: npm', async t => {
@@ -18,8 +15,15 @@ test('user: npm', async t => {
 	t.is(user.name, 'No Problem, Meatbag');
 	t.regex(user.avatar, /gravatar\.com\/avatar/);
 	t.is(user.email, 'npm@npmjs.com');
-	t.is(user.homepage, 'https://npmjs.org');
 	t.is(user.github, 'npm');
 	t.is(user.twitter, 'npmjs');
-	t.is(user.freenode, null);
+});
+
+test('user: tj', async t => {
+	const user = await m('tj');
+	t.is(user.name, null);
+	t.regex(user.avatar, /gravatar\.com\/avatar/);
+	t.is(user.email, 'tj@vision-media.ca');
+	t.is(user.github, null);
+	t.is(user.twitter, null);
 });
